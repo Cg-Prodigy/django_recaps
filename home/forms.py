9c8ba878463564a2e71b71ae5848c9208a0ab5e3
Cg-Prodigy <egomaritmbrian@gmail.com>
2022-11-03 19:00:04 +0300

    Advance form features

--- home/forms.py
+++ home/forms.py
@@ -1,4 +1,13 @@
+import re
 from django import forms
+from django.core.exceptions import ValidationError
+
+# custom validators
+def validate_password(value):
+    regx=re.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$")
+    if not re.search(regx,value):
+        raise ValidationError("Password is weak!")
+
 
 class LoginForm(forms.Form):
     CAREER_CHOICES=(
@@ -26,5 +35,20 @@ class LoginForm(forms.Form):
     sub_careers=forms.MultipleChoiceField(choices=CAREER_CHOICES)
     years_of_experience=forms.IntegerField()
     about_yourself=forms.CharField(widget=forms.Textarea(attrs={'rows':3}))
-    password=forms.CharField(widget=forms.PasswordInput)
-    c_password=forms.CharField(widget=forms.PasswordInput)
+    password=forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Create a strong password."}), validators=[validate_password],
+    help_text="""
+    <ul>
+    <li>Should have at least one number.</li>
+    <li>Should have at least one uppercase and one lowercase character.</li>
+    <li>Should have at least one special symbol.</li>
+    <li>Should be between 6 to 20 characters long.</li>
+    </ul>"""
+    )
+    c_password=forms.CharField(widget=forms.PasswordInput,validators=[validate_password])
+    def clean_first_name(self):
+        value=self.cleaned_data['first_name'].strip()
+        return value.title()
+    def clean(self):
+        data=super().clean()
+        if data.get('password')!=data.get('c_password'):
+            self.add_error('c_password',"Passwords do not match!")
